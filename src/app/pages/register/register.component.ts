fc614c7840d2a3bbd8f054e87c9dee41e9b87206
Sonia <soniasophiaw@gmail.com>
2017-06-01 10:26:26 -0700

    Bind formGroup and validators to register form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -21,6 +21,8 @@ export class RegisterComponent implements OnInit {
   colonistJobId: string = 'no job';
   public colonist: Colonist;
 
+  registerForm: FormGroup;
+
   constructor(private jobService: JobsService, private colonistService: ColonistService) { }
 
   ngOnInit() {
@@ -28,11 +30,18 @@ export class RegisterComponent implements OnInit {
     .subscribe((data) => {
       this.jobs = data.jobs;
     });
+
+    this.registerForm = new FormGroup ({
+      name: new FormControl('', [Validators.required, Validators.maxLength(100), Validators.minLength(3)]),
+      age: new FormControl('', [Validators.required]),
+      job_id: new FormControl('', [])
+    });
   }
 
   postColonist() {
     const colonist = new Colonist(this.colonistName, this.colonistAge, this.colonistJobId);
     this.colonistService.postData(colonist)
-      .subscribe((newColonist) => {});
+      .subscribe((newColonist) => {
+      });
   }
 }
